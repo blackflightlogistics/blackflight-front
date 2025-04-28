e72ce0be8e2136061ccccd8aa0e41bc5a751c012
lucasde almeida <lucasdealmeidadev@gmail.com>
2025-04-28 09:53:29 -0300

    adicionada busca fake

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import {   useState } from "react";
+import {   useRef, useState } from "react";
 import Navbar from "../components/Navbar";
 // import { Encomenda } from "../services/encomendaService";
 import Header from "../components/home/Header";
@@ -7,18 +7,31 @@ import Hero from "../components/home/Hero";
 import ServicesSection from "../components/home/ServicesSection";
 // import WhoWeAre from "../components/home/WhoWeAre";
 import FrequentlyQuestion from "../components/home/FrequentlyQuestion";
-import HomeFooter from "../components/home/HomeFooter";
+// import HomeFooter from "../components/home/HomeFooter";
 import { FiSearch } from "react-icons/fi";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaYoutube,
+  FaPaperPlane,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Home() {
   // const resultadoRef = useRef<HTMLDivElement>(null);
   const [codigoDigitado, setCodigoDigitado] = useState("");
   const [resultado, setResultado] = useState(false);
+  const resultadoRef = useRef<HTMLDivElement>(null);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (codigoDigitado.trim() !== "") {
-      setResultado(true); // simula uma resposta de rastreio
+      setResultado(true);
+  
+      setTimeout(() => {
+        resultadoRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+      }, 100); // pequeno delay para garantir que o DOM atualizou
     }
   };
 
@@ -111,7 +124,7 @@ function Home() {
 
               {/* Resultado */}
               {resultado && (
-                <div className="text-left space-y-4">
+                <div ref={resultadoRef}  className="text-left space-y-4">
                   <div className="flex items-center justify-between text-sm">
                     <div>
                       <span className="font-semibold">
@@ -182,7 +195,105 @@ function Home() {
 
       <ServicesSection />
       <FrequentlyQuestion />
-      <HomeFooter />
+      <footer className="bg-black text-white pt-16 pb-8 px-6">
+      <div className="max-w-7xl mx-auto grid md:grid-cols-4 gap-12">
+        {/* Logo e redes sociais */}
+        <div>
+          <div className="mb-4">
+            <Link to="/" className="text-xl font-bold text-black ">
+              <img src="image.png" alt="logo" className="w-[3rem] h-[2rem]" />
+            </Link>
+          </div>
+          <div className="flex gap-4">
+            <a
+              href="#"
+              className="bg-black border border-orange p-2 rounded-full hover:bg-orange transition"
+            >
+              <FaFacebookF className="text-orange hover:text-black text-sm" />
+            </a>
+            <a
+              href="#"
+              className="bg-black border border-orange p-2 rounded-full hover:bg-orange transition"
+            >
+              <FaInstagram className="text-orange hover:text-black text-sm" />
+            </a>
+            <a
+              href="#"
+              className="bg-black border border-orange p-2 rounded-full hover:bg-orange transition"
+            >
+              <FaLinkedinIn className="text-orange hover:text-black text-sm" />
+            </a>
+            <a
+              href="#"
+              className="bg-black border border-orange p-2 rounded-full hover:bg-orange transition"
+            >
+              <FaYoutube className="text-orange hover:text-black text-sm" />
+            </a>
+          </div>
+        </div>
+
+        {/* Serviços */}
+        <div>
+          <h3 className="font-semibold font-primary text-white mb-4">
+            <span className="border-b-2 border-orange pb-1">Serviços</span>
+          </h3>
+          <ul className="space-y-2 text-sm font-normal font-secondary text-lightGrey">
+            <li>Envio de pacotes</li>
+            <li>Rastreamento online</li>
+            <li>Atendimento personalizado</li>
+          </ul>
+        </div>
+
+        {/* Links úteis */}
+        <div>
+          <h3 className="font-semibold font-primary text-white mb-4">
+            <span className="border-b-2 border-orange pb-1">Links Úteis</span>
+          </h3>
+          <ul className="space-y-2 text-sm font-normal font-secondary text-lightGrey">
+            <li>Rastreie sua encomenda</li>
+            <li>Quem somos</li>
+            <li>Nossos serviços</li>
+          </ul>
+        </div>
+
+        {/* Rastreio rápido */}
+        <div>
+          <h3 className="font-semibold font-primary text-white mb-4">
+            <span className="border-b-2 border-orange pb-1">
+              Rastreie sua encomenda
+            </span>
+          </h3>
+          <p  className="space-y-2 text-sm font-normal font-secondary text-lightGrey">
+            Insira o código de rastreio
+          </p>
+          <form
+            onSubmit={handleSubmit}
+            className="flex overflow-hidden rounded-md"
+          >
+            <input
+              name="codigo"
+              value={codigoDigitado}
+              onChange={(e) => setCodigoDigitado(e.target.value)}
+
+              placeholder="Código de rastreio"
+              className="flex-1 px-3 py-2 text-sm text-black outline-none"
+            />
+            <button
+              type="submit"
+              className="bg-orange text-white px-4 flex items-center justify-center"
+            >
+              <FaPaperPlane size={14} />
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <hr className="my-8 border-white/10" />
+
+      <p className="text-xs text-center text-white/70">
+        © 2025 Sistema de envio de pacotes. Todos os direitos reservados.
+      </p>
+    </footer>
     </div>
   );
 }
